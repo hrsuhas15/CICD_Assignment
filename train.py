3362c11a03516d95e7322580b3b96ce96a128534
hrsuhas15 <hrsuhas15@gmail.com>
2024-04-02 09:10:47 +0530

    changes

--- train.py
+++ train.py
@@ -1,39 +1,15 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
 
 df = pd.read_csv("data/train.csv")
-X = df.drop(columns=['Disease'])
-y = df['Disease']
+X = df.drop(columns=['Disease']).to_numpy()
+y = df['Disease'].to_numpy()
+labels = np.sort(np.unique(y))
+y = np.array([np.where(labels == x) for x in y]).flatten()
 
-#model = LogisticRegression().fit(X, y)
-# Define preprocessing steps for numerical and categorical features
-numeric_features = X.select_dtypes(include=['int64', 'float64']).columns
-numeric_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='mean')),
-    ('scaler', StandardScaler())])
-
-categorical_features = X.select_dtypes(include=['object']).columns
-categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
-
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', numeric_transformer, numeric_features),
-        ('cat', categorical_transformer, categorical_features)])
-
-# Define the model
-model = Pipeline(steps=[('preprocessor', preprocessor),
-                        ('classifier', RandomForestClassifier(random_state=42))])
-
-# Train the model
+model = LogisticRegression(max_iter=1000, solver='lbfgs')
 model.fit(X, y)
 
 with open("model.pkl", 'wb') as f:
