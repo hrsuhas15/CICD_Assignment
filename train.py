c0f3850fad548e83b92196f7dbb90673a3759e2b
hrsuhas15 <hrsuhas15@gmail.com>
2024-04-02 08:48:17 +0530

    added changes

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import pickle
 import numpy as np
 
@@ -10,8 +12,16 @@ labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
 #model = LogisticRegression().fit(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize numerical features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Train the model
 model = LogisticRegression(random_state=42, max_iter=1000)
-model.fit(X, y)
+model.fit(X_train_scaled, y_train)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
