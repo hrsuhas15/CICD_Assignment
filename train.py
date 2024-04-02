32780dffcfdc3f96a001dface2057148a4281401
hrsuhas15 <hrsuhas15@gmail.com>
2024-04-02 09:06:14 +0530

    changes

--- train.py
+++ train.py
@@ -9,10 +9,8 @@ import pickle
 import numpy as np
 
 df = pd.read_csv("data/train.csv")
-X = df.drop(columns=['Disease']).to_numpy()
-y = df['Disease'].to_numpy()
-labels = np.sort(np.unique(y))
-y = np.array([np.where(labels == x) for x in y]).flatten()
+X = df.drop(columns=['Disease'])
+y = df['Disease']
 
 #model = LogisticRegression().fit(X, y)
 # Define preprocessing steps for numerical and categorical features
